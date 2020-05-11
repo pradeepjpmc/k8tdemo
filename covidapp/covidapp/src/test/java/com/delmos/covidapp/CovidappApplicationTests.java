fe01cf12b00fcf5edb47f0ea163438806bb73594
Pradeep <tech.pkgupta92@gmail.com>
2020-05-12 00:27:56 +0530

    intial commit for this app

---
+++ covidapp/covidapp/src/test/java/com/delmos/covidapp/CovidappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delmos.covidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
